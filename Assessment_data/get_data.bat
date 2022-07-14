4e5cf6d51471d603361349c28d97eb6b4f2951ff
MatthVeron <71879012+MatthVeron@users.noreply.github.com>
2022-07-13 17:11:50 -0700

    Update get_data.bat

--- Assessment_data/get_data.bat
+++ Assessment_data/get_data.bat
@@ -8,16 +8,13 @@ set "BBRKC=\build\BBRKC\"
 set "SMBKC=\build\SMBKC\"
 set "WAG=\build\AIGKC\WAG\"
 set "EAG=\build\AIGKC\EAG\"
-set "SNOW=\build\SNOW\"
-set "SNOW_MTV=\build\SNOW_M_time_varying\"
+set "SNOW_crab=\build\SNOW_crab\"
 
 set "DirBBRKC= "%dir%%Ver%%BBRKC%""
 set "DirSMBKC= "%dir%%Ver%%SMBKC%""
 set "DirWAG= "%dir%%Ver%%WAG%""
 set "DirEAG= "%dir%%Ver%%EAG%""
-set "DirSNOW= "%dir%%Ver%%SNOW%""
-set "DirSNOW_MTV= "%dir%%Ver%%SNOW_MTV%""
-
+set "DirSNOW= "%dir%%Ver%%SNOW_crab%""
 
 copy BBRKC\bbrkc21.prj %DirBBRKC%
 copy BBRKC\bbrkc21-193d.dat %DirBBRKC%
@@ -29,23 +26,16 @@ copy SMBKC\sm22.DAT %DirSMBKC%
 copy SMBKC\sm22.CTL %DirSMBKC%
 copy SMBKC\gmacs.DAT %DirSMBKC%
 
-
 copy AIGKC\EAG\gmacs8cEAG21_1eUpdate2CatchNo.PRJ %DirEAG%
 copy AIGKC\EAG\Gmacs8cEAG21_1eUpdate2CatchNo.DAT %DirEAG%
 copy AIGKC\EAG\Gmacs8cEAG21_1eUpdate2CatchNo.CTL %DirEAG%
 copy AIGKC\EAG\gmacs.DAT %DirEAG%
-
-
 
 copy AIGKC\WAG\gmacs8cWAG21_1eUpdate2CatchNo.PRJ %DirWAG%
 copy AIGKC\WAG\gmacs8cWAG21_1eUpdate2CatchNo.DAT %DirWAG%
 copy AIGKC\WAG\Gmacs8cWAG21_1eUpdate2CatchNo.CTL %DirWAG%
 copy AIGKC\WAG\gmacs.DAT %DirWAG%
 
-copy SNOW\*.prj %DirSNOW%
-copy SNOW\*.dat %DirSNOW%
-copy SNOW\*.ctl %DirSNOW%
-
-copy SNOW_M_time_varying\*.prj %DirSNOW_MTV%
-copy SNOW_M_time_varying\*.dat %DirSNOW_MTV%
-copy SNOW_M_time_varying\*.ctl %DirSNOW_MTV%
+copy SNOW_crab\*.prj %DirSNOW%
+copy SNOW_crab\*.dat %DirSNOW%
+copy SNOW_crab\*.ctl %DirSNOW%
